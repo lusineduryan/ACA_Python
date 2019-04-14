8822f46408fadcc6e367a08a04d789b15b7d8999
lusine duryan <lusineduryan@mail.ru>
2019-04-14 13:25:03 +0400

    minor adjustments

--- Basics/Classwork_2.py
+++ Basics/Classwork_2.py
@@ -2,7 +2,10 @@ input_number = input()
 
 int_number = int(input_number, base = 2)
 
+int_number_1 = int(input_number, base = 10)
+
 print(int_number)
+print(int_number_1)
 
 count = 0
 
@@ -20,9 +23,10 @@ int_number = int_number // 2
 
 count += int_number
 
-count_short = int_number % 2 + int_number // 2 % 2 + int_number // 4 % 2 + int_number // 8 % 2 + int_number // 16 % 2
+count_short = int_number_1 % 2 + int_number_1 // 2 % 2 + int_number_1 // 4 % 2 + int_number_1 // 8 % 2 + int_number_1 \
+              // 16 % 2
 
 
 
-print(count)
-print(count_short)
+print("Count: ", count)
+print("Count_short: ", count_short)
