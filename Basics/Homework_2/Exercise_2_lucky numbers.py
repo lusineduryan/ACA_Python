f959d74f45925f61da629913f9adfe8d448f1f32
lusine duryan <lusineduryan@mail.ru>
2019-04-23 20:02:07 +0400

    homework_2 completed

---
+++ Basics/Homework_2/Exercise_2_lucky numbers.py
@@ -0,0 +1,9 @@
+# Call a number lucky if the sum of its digits on even positions is equal to the sum of its digits on odd positions.
+# Given a number in decimal system(base 10): a. It’s length is 5 digits. Output True if a is lucky, otherwise False.
+
+input_number = input()
+
+int_number_coll = (int(input_number[0]), int(input_number[1]), int(input_number[2]), int(input_number[3]), int(input_number[4]))
+
+print(sum(int_number_coll[::2]) == sum(int_number_coll[1::2]))
+
