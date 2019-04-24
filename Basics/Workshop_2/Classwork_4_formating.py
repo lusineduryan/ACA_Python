d5c3f233fbf5f2d19920e64ad547af99c4c2e9e0
lusine duryan <lusineduryan@mail.ru>
2019-04-24 23:41:28 +0400

    minor changes

--- Basics/Workshop_2/Classwork_4_formating.py
+++ Basics/Workshop_2/Classwork_4_formating.py
@@ -5,6 +5,7 @@ age = int(input())
 height = float(input())
 
 height_format = '{:04.2f}'.format(height)
+
 print(f"Hi, my name is {name}. My age is {age} and my height is {height_format}.")
 
 print("Hi, my name is %s. My age is %d and my height is %.2f." % (name, age, height))
