abc1e2a6e5d5c47c178b1b031a43fdccbecc2dd2
lusine duryan <lusineduryan@mail.ru>
2019-05-03 19:04:33 +0400

    Goldbach Conjecture with functions added

--- Basics/Homework_3/Exercise_2_goldbach conjecture with functions.py
+++ Basics/Homework_3/Exercise_2_goldbach conjecture with functions.py
@@ -4,4 +4,16 @@
 input_number = int(input())
 
 def isPrime (arg):
-    
+    for i in range(2, arg + 1):
+        flag = True
+        if arg % i == 0:
+            for j in range(2, i):
+                if i % j == 0:
+                    flag = False
+                    break
+    return flag
+
+for i in range(2, input_number + 1):
+    if isPrime(i) == True and isPrime(input_number - i) == True:
+        print(i, input_number - i)
+        break
