c74b3ad5dfe6cdfd062c7db26a27a5a3384022bd
lusine duryan <lusineduryan@mail.ru>
2019-05-05 01:08:40 +0400

    modules added

---
+++ Basics/Sololearn/Functions and modules/Modules.py
@@ -0,0 +1,13 @@
+import random
+
+for i in range(5):
+    value = random.randint(2, 10)
+    print(value)
+
+from math import pi
+
+print(pi)
+
+from math import sqrt as square_root
+
+print(square_root(4))
