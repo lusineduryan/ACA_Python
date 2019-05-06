c9366427a7b0338642ecd1d3a57fad85f9b5a27c
lusine duryan <lusineduryan@mail.ru>
2019-05-06 14:36:48 +0400

    missing file added

---
+++ Basics/Sololearn/Basic Concepts/Simple operations & strings.py
@@ -0,0 +1,23 @@
+a = 15
+b = 8
+c = "Lucy"
+d = "Duryan"
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a ** b)
+print(a % b)
+
+print("Lucy\'s surname is Duryan")
+print("""Lucy
+Duryan""")
+
+print("Lucy\nDuryan")
+print("Lucy\\nDuryan")
+print(c + d)
+print((c + " ") * 3)
+
+
