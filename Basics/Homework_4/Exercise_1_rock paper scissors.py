a6bfa012f8de28976dfe20bfa8e1d082cdd04f97
lusine duryan <lusineduryan@mail.ru>
2019-05-07 17:34:30 +0400

    ex 4 added

---
+++ Basics/Homework_4/Exercise_1_rock paper scissors.py
@@ -0,0 +1,3 @@
+# Make a two-player Rock-Paper-Scissors game. User inputs 3 for Rock, 2 for paper, 1 for scissors and 0 to end the game.
+# After each step show game result(for example human: 4 vs 4 :PC).
+
--- Basics/W3schools/Syntax.py
+++ Basics/W3schools/Syntax.py
@@ -3,3 +3,7 @@ print("Hello world!")
 # this is a comment
 
 print("this is for github update:)")
+
+
+
+
