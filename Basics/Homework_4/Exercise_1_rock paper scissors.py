6fb7560f983c5f0c68cfde0f20fa79eb268fd218
lusine duryan <lusineduryan@mail.ru>
2019-05-07 18:30:54 +0400

    rock paper scissors started

--- Basics/Homework_4/Exercise_1_rock paper scissors.py
+++ Basics/Homework_4/Exercise_1_rock paper scissors.py
@@ -1,3 +1,14 @@
 # Make a two-player Rock-Paper-Scissors game. User inputs 3 for Rock, 2 for paper, 1 for scissors and 0 to end the game.
 # After each step show game result(for example human: 4 vs 4 :PC).
 
+import random
+
+def game_result(arg1, arg2):
+    '3' > '1'
+    '2' > '3'
+    '1' > '2'
+    return int(arg1 > arg2)
+
+input_number = int(input())
+while int(input_number) != 0:
+    input_number = input()
