9614f48ce816a150c1f3ab0624efaf3befcc0b09
lusine duryan <lusineduryan@mail.ru>
2019-05-19 15:04:50 +0400

    workshop 5 added

--- Basics/Workshop_5/Classwork_2_factorial dict rec.py
+++ Basics/Workshop_5/Classwork_2_factorial dict rec.py
@@ -4,6 +4,7 @@ def factorial(n):
     if n in fact:
         return fact[n]
     else:
-        return factorial(n - 1) * n
+        fact[n] = factorial(n - 1) * n
+        return fact[n]
 
 print(factorial(int(input())))
