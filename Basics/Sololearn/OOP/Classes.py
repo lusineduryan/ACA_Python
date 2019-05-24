c38e00db7a5d6e088f9492dbdda8904005c2e3a3
lusine duryan <lusineduryan@mail.ru>
2019-05-24 18:16:45 +0400

    classes added

---
+++ Basics/Sololearn/OOP/Classes.py
@@ -0,0 +1,23 @@
+class Cat:
+    def __init__(self, color, legs):
+        self.color = color
+        self.legs = legs
+
+felix = Cat("ginger", 4)
+print(felix.color)
+
+class Dog:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+    def bark(self):
+        print("Woof!")
+
+    legs = 4
+
+fido = Dog("Fido", "brown")
+print(fido.name)
+fido.bark()
+print(fido.legs)
+print(Dog.legs)
