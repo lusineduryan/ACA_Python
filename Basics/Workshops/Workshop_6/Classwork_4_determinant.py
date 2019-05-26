d28f34482812a5dfffcfb1a711da8a4cbb0da332
lusine duryan <lusineduryan@mail.ru>
2019-05-26 15:06:02 +0400

    workshop 6 added

---
+++ Basics/Workshops/Workshop_6/Classwork_4_determinant.py
@@ -0,0 +1,13 @@
+def deter(arg):
+    assert len(arg) == len(arg[0])
+    if len(arg) == 2:
+        return arg[0][0] * arg[1][1] - arg[0][1] * arg[1][0]
+    else:
+        det = 0
+        for i in arg[0]:
+            m = arg.remove(i).remove(arg[1][0]).remove(arg[2][0])
+            det += (-1) * (i.inde + 1) * det(m)
+            return det
+
+A = [[1,5,-1], [1,2,3], [2,3,4]]
+print(deter(A))
