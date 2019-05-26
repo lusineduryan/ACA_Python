1dba4ade053619b6e1ef7bc61bc9e7ba500deb81
lusine duryan <lusineduryan@mail.ru>
2019-05-26 14:19:38 +0400

    workshop 6 added

--- Basics/Workshops/Workshop_6/Classwork_3_T-matrix.py
+++ Basics/Workshops/Workshop_6/Classwork_3_T-matrix.py
@@ -1,3 +1,5 @@
+from .Classwork_2_scalarProduct import scal
+
 def trans(arg):
     res = []
     for i in range(len(arg[0])):
@@ -7,4 +9,20 @@ def trans(arg):
         res.append(line)
     return res
 
-print(trans([[1,1,1], [2,2,2]]))
+print(*trans([[1,1,1], [2,2,2]]), sep = '\n')
+
+a = [[1,1],[2,2]]
+b = [[3,4],[3,4]]
+
+def mat_prod(arg1, arg2):
+    tr = trans(arg2)
+    assert len(arg1[0]) == len(arg2[0])
+    res = []
+    for i in arg1:
+        line = []
+        for j in tr:
+            line.append(scal(i, j))
+        res.append(line)
+    return res
+
+print(mat_prod(a,b))
