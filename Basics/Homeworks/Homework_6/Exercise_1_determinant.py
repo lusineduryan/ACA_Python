657cd3f904ebb883b88533c96d2619cdc82b42c0
lusine duryan <lusineduryan@mail.ru>
2019-05-31 15:36:18 +0400

    determinant started

--- Basics/Homeworks/Homework_6/Exercise_1_determinant.py
+++ Basics/Homeworks/Homework_6/Exercise_1_determinant.py
@@ -3,11 +3,18 @@ def deter(arg):
     if len(arg) == 2:
         return arg[0][0] * arg[1][1] - arg[0][1] * arg[1][0]
     else:
-        det = 0
-        for i in arg[0]:
-            m = arg.remove(i).remove(arg[1][0]).remove(arg[2][0])
-            det += (-1) * (i.inde + 1) * det(m)
-            return det
+        d = 0
+        minor = []
+        for i in range(len(arg)):
+            minor_row = []
+            for j in range(len(arg[i])):
+                if j != i and j != 0:
+                    minor_row.append(arg[i][j])
+            minor.append(minor_row)
+            d += (-1) ** i * i * deter(minor)
+        return d
 
-A = [[1,5,-1], [1,2,3], [2,3,4]]
+A = [[0,1,2], [2,3,4], [3,4,6]]
+B = [[1,2],[3,4]]
+print(deter(B))
 print(deter(A))
