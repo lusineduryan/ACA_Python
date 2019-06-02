5159fdbb899485a874175976251ca337149035a3
lusine duryan <lusineduryan@mail.ru>
2019-06-02 14:20:09 +0400

    workshop 7 added

--- Basics/Workshops/Workshop_7/Classwork_1.py
+++ Basics/Workshops/Workshop_7/Classwork_1.py
@@ -2,5 +2,12 @@ import numpy as np
 
 a = list(range(100))
 print(np.array(a).shape)
-print(np.array(a).reshape(10,10))
-print(np.array(a).shape)
+b = np.array(a).reshape(10,10)
+print(b, b.shape)
+
+a_2 = np.array([[1,2,3], [8,10,15],[8,9,2]])
+print(a_2[:, 0])
+print(a_2[1])
+
+print(a_2[0,: -1])
+print(a_2[:-1, :-1])
