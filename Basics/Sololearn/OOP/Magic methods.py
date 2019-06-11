f92d4f996b7cbd9c5a25c332848b03337680ce1c
lusine duryan <lusineduryan@mail.ru>
2019-06-11 17:33:37 +0400

    magic methods added

--- Basics/Sololearn/OOP/Magic methods.py
+++ Basics/Sololearn/OOP/Magic methods.py
@@ -10,3 +10,22 @@ second = Vector2D(3, 9)
 result = first + second
 print(result.x)
 print(result.y)
+
+class SpecialString:
+    def __init__(self, cont):
+        self.cont = cont
+
+    def __truediv__(self, other):
+        line = '=' * len(other.cont)
+        return '\n'.join([self.cont, line, other.cont])
+
+    def __gt__(self, other):
+        for i in range(len(other.cont) + 1):
+            result = other.cont[:i] + '>' + self.cont
+            result += '>' + other.cont[i:]
+            print(result)
+
+lucy = SpecialString('Lucy')
+duryan = SpecialString('Duryan')
+print(lucy / duryan)
+lucy > duryan
