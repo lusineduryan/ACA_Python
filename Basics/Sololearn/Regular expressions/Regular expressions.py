a489baeb2bf17c1b0bebb16cc8e6a36d86d2b408
lusine duryan <lusineduryan@mail.ru>
2019-06-12 14:32:13 +0400

    regular expressions added

--- Basics/Sololearn/Regular expressions/Regular expressions.py
+++ Basics/Sololearn/Regular expressions/Regular expressions.py
@@ -9,6 +9,10 @@ else:
 
 if re.search(pattern, 'eggspamspams'):
     print('Search result found')
+    print(re.search(pattern, 'eggspamspams').group())
+    print(re.search(pattern, 'eggspamspams').start())
+    print(re.search(pattern, 'eggspamspams').end())
+    print(re.search(pattern, 'eggspamspams').span())
 else:
     print('No search result found')
 
